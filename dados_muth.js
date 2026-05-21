522cf6d17ad139243c651fa52c89d6bb9978a653
meirelesnew <muty.bjj@gmail.com>
2026-05-21 15:37:00 -0300

    Delete dados_muth.js

--- dados_muth.js
+++
@@ -1,7 +0,0 @@
-// Gerado automaticamente pela Muth via Telegram
-const dadosMuth = {
-    ativo: "BTC-USD",
-    decisao: "AGUARDAR ⏳",
-    confianca: "47.8%",
-    ultimaAtualizacao: "18/05/2026 11:04:10"
-};
